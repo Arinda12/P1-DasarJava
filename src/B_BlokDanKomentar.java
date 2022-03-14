54ea26d86df249772c5c64b703769d7236fabbae
PC 13 <arindahafidah@gmail.com>
2022-03-14 02:28:46 -0700

    Inintial project pertemuan 1,membuat blok dan komentar di java

---
+++ src/B_BlokDanKomentar.java
@@ -0,0 +1,8 @@
+/**
+ * Contoh penggunaan whitepace di java
+ */
+public class B_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek Dengan Java");
+    }
+}
